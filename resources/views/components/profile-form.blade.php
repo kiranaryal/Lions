beaa4fa8aab3cb1b6a3b7fa0b77d7b4b7a87ae1c
kiranaryal <kiranaryal2057@gmail.com>
2024-03-15 13:37:44 +0545

    news

--- resources/views/components/profile-form.blade.php
+++ resources/views/components/profile-form.blade.php
@@ -5,7 +5,7 @@
 
     <div class="mt-5 md:mt-0 md:col-span-7">
         <form wire:submit="{{ $submit }}">
-            <div class="px-4 py-5 bg-sky-100  dark:bg-gray-800 sm:p-6 shadow-lg min-h-screen rounded-3xl {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
+            <div class="px-4 py-5 bg-sky-100  dark:bg-gray-800 sm:p-6 shadow-lg  rounded-3xl {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
                 <div class="grid grid-cols-12 gap-6 ">
                     {{ $form }}
                 </div>
