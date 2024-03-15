bdf9d77a37f0396a30b2d7f714dde5deca7eda7b
kiranaryal <kiranaryal2057@gmail.com>
2024-03-15 13:18:19 +0545

    news

--- resources/views/components/profile-form.blade.php
+++ resources/views/components/profile-form.blade.php
@@ -1,11 +1,11 @@
 @props(['submit'])
 
-<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-10 h-full md:gap-6']) }}>
+<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-10  md:gap-6']) }}>
 
 
     <div class="mt-5 md:mt-0 md:col-span-7">
         <form wire:submit="{{ $submit }}">
-            <div class="px-4 py-5 bg-sky-100  dark:bg-gray-800 sm:p-6 shadow-lg rounded-3xl {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
+            <div class="px-4 py-5 bg-sky-100  dark:bg-gray-800 sm:p-6 shadow-lg min-h-screen rounded-3xl {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
                 <div class="grid grid-cols-12 gap-6 ">
                     {{ $form }}
                 </div>
