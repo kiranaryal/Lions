a991c34b48334885926125b202027514d4efdb27
kiranaryal <kiranaryal2057@gmail.com>
2024-03-15 13:09:35 +0545

    news

--- resources/views/livewire/news.blade.php
+++ resources/views/livewire/news.blade.php
@@ -3,15 +3,20 @@
         <div class="text-center mt-5 text-xl font-bold pt-5"> News Feed</div>
         @foreach ($this->news as $new)
             <div class="m-5 p-5 bg-white rounded-3xl shadow-lg flex flex-col">
-                <div class="flex items-center ">
-                    <img src="{{$new->getImage()}}" alt="" class="h-16 w-16 rounded-full object-cover ">
+                <div class="flex space-x-10 ">
+
+                    <div class="flex items-center  ">
+                        <img src="{{$new->getImage()}}" alt="" class="h-16 w-16 rounded-full object-cover ">
                     <div class="pl-5">
                         <p class="font-bold text-lg">{{$new->writer}}</p>
                         <p class="text-sm">{{$new->date}}</p>
                     </div>
                 </div>
-                <div class="p-5 pt-0">
-                    <p class="text-center font-bold text-lg">{{$new->title}}</p>
+                <div class="pl-5 pt-2 text-conter">
+                    <p class="text-center font-bold text-xl">{{$new->title}}</p>
+                </div>
+                </div>
+                <div class="p-5 ">
                     <p class="text-justify">{!!$new->content!!}</p>
                 </div>
             </div>
