ff983c81231d1dc2fd419c9cff8f9f4460843673
kiranaryal <kiranaryal2057@gmail.com>
2024-03-28 11:35:07 +0545

    removed dark all

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
         './vendor/wire-elements/modal/resources/views/*.blade.php',
 
     ],
+    darkMode: false,
     options: {
         safelist: {
               pattern: /max-w-(sm|md|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl)/,
